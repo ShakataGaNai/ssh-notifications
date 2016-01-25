33c7be299a38719533d5c11ff3995fa5587495b6
Jon Davis <jdavis@trueaccord.com>
2016-01-25 11:25:47 -0800

    Original script from http://sandrinodimattia.net/posting-successful-ssh-logins-to-slack/

---
+++ notify.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ "$PAM_TYPE" != "close_session" ]; then
+        url="YOUR_SLACK_WEBHOOK_URL"
+        channel="#ssh-logins"
+        host="`hostname`"
+    content="\"attachments\": [ { \"mrkdwn_in\": [\"text\", \"fallback\"], \"fallback\": \"SSH login: $PAM_USER connected to \`$host\`\", \"text\": \"SSH login to \`$host\`\", \"fields\": [ { \"title\": \"User\", \"value\": \"$PAM_USER\", \"short\": true }, { \"title\": \"IP Address\", \"value\": \"$PAM_RHOST\", \"short\": true } ], \"color\": \"#F35A00\" } ]"
+    curl -X POST --data-urlencode "payload={\"channel\": \"$channel\", \"mrkdwn\": true, \"username\": \"ssh-bot\", $content, \"icon_emoji\": \":computer:\"}" $url
+fi  
